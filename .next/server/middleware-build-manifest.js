1f952b9c6bde474a7ab67c2f99b1461f4cc4b00f
Michael <mchaelmorris@gmail.com>
2025-09-17 17:40:08 -0400

    pad

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -10,11 +10,6 @@ globalThis.__BUILD_MANIFEST = {
   "rootMainFiles": [],
   "rootMainFilesTree": {},
   "pages": {
-    "/": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/index.js"
-    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -29,16 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/dashboard.js"
-    ],
-    "/login": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/login.js"
-    ],
-    "/register": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/register.js"
     ]
   },
   "ampFirstPages": []
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -17,7 +17,7 @@ const PageWrapper = styled.div`
 
 const Hero = styled.section`
   max-width: 700px;
-  margin: 1rem 1.8rem;
+  margin: 3rem 1.8rem;
   text-align: center;
 `
 
